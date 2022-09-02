322bf7ec3bdb2713e17b12edaf018cd70a35104c
emma673r <90177139+emma673r@users.noreply.github.com>
2022-09-02 13:38:41 +0200

    barArray colors

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-`use strict`
+"use strict";
 
-window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", init);
 
-// all const
+const model = [];
+const barArray = document.querySelectorAll(".bar");
 
+let timeoutSpeed = 500;
 
-// all variables
-let queueSize = getNumberOfCustomers();
+function init() {
+  console.log("init");
+  loop();
+}
 
-function start() {
+// Controller
+function loop() {
+  arrUpdate();
+  displayData();
+  setTimeout(loop, timeoutSpeed);
+}
 
+// Model
+function arrUpdate() {
+  if (model.length < 40) {
+    model.unshift(getNumberOfCustomers());
+  } else {
+    model.length = 39;
+    model.unshift(getNumberOfCustomers());
+  }
+  console.log(model);
+}
+
+// View
+function displayData() {
+  for (let i = 0; i < 40; i++) {
+    barArray.textContent = model[i];
+    barArray[i].style.height = `${model[i]}px`;
+    if (model[i] < 15) {
+      barArray[i].style.backgroundColor = `yellowgreen`;
+    } else if (model[i] < 25) {
+      barArray[i].style.backgroundColor = `yellow`;
+    } else if (model[i] < 35) {
+      barArray[i].style.backgroundColor = `orange`;
+    } else {
+      barArray[i].style.backgroundColor = `red`;
+    }
+  }
 }
 
 function getNumberOfCustomers() {
-    // FAKE : goves a completely random number since no access to real queue data
-    return Math.floor(Math.random()*32);
+  return Math.floor(Math.random() * 32);
 }
